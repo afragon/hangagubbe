b4e25de9ce3739b13036f6f11988bc557b11f704
Afra Noubarzadeh <afra.noubarzadeh@gmail.com>
2022-04-01 15:42:41 +0200

    Update script.js

--- script.js
+++ script.js
@@ -20,6 +20,7 @@ const theWordEL = document.querySelector('.theWord');
 const WrongWordsdEL = document.querySelector('.wrongWords');
 const infoEl = document.querySelector('.info');
 const ownWordEl = document.getElementById('decide');
+const decideBarEl = document.getElementById('submitOwnWord');
 
 let decideWord, theWord, arrTheWord, secretWord, guess, life, points;
 
@@ -68,12 +69,18 @@ const init = function () {
 };
 
 ownWordEl.addEventListener('click', function () {
-  theWord = prompt('wooord?');
-  arrTheWord = [...theWord];
-  secretWord = [];
-  calcChallenge(arrTheWord);
-  displayWord(secretWord.join(' '));
-  displayLife(life);
+  decideBarEl.classList.remove('hidden');
+});
+
+decideBarEl.addEventListener('keydown', function (e) {
+  if (e.key == 'Enter' && !decideBarEl.value.length == 0) {
+    theWord = decideBarEl.value.toLowerCase();
+    arrTheWord = [...theWord];
+    secretWord = [];
+    calcChallenge(arrTheWord);
+    displayWord(secretWord.join(' '));
+    displayLife(life);
+  }
 });
 
 ////////////////////////////////////
@@ -131,4 +138,3 @@ let test2 = function (arr, guess) {
 };
 
 resetBtn.addEventListener('click', init);
-

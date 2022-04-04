77dc7a798bf8a3b824f74e488b8d66f7c2bc4a17
Afra Noubarzadeh <afra.noubarzadeh@gmail.com>
2022-04-04 11:09:59 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,32 @@
-"use strict";
+'use strict';
 
 const dictonary = test;
 
-const guessBar = document.getElementById("submitWord");
-const submitBtn = document.getElementById("submit");
-const resetBtn = document.getElementById("reset");
-const currentLife = document.querySelector(".life");
-const theWordEL = document.querySelector(".theWord");
-const WrongWordsdEL = document.querySelector(".wrongWords");
-const infoEl = document.querySelector(".info");
-const ownWordEl = document.getElementById("decide");
-const decideBarEl = document.getElementById("submitOwnWord");
+const guessBar = document.getElementById('submitWord');
+const submitBtn = document.getElementById('submit');
+const resetBtn = document.getElementById('reset');
+const currentLife = document.querySelector('.life');
+const theWordEL = document.querySelector('.theWord');
+const WrongWordsdEL = document.querySelector('.wrongWords');
+const infoEl = document.querySelector('.info');
+const ownWordEl = document.getElementById('decide');
+const decideBarEl = document.getElementById('submitOwnWord');
 
 let decideWord, theWord, arrTheWord, secretWord, guess, life, points;
 
-let chosenWords = ["⛔️"];
+let chosenWords = ['⛔️'];
 
 ////////////////////////////////////
 ////////////////////////////////////
 ////////////////////////////////////
 const calcChallenge = function (arr) {
   for (let i = 0; i < arr.length; i++) {
-    secretWord.push("_");
+    secretWord.push('_');
   }
 };
 
 const displayWord = function (message) {
-  theWordEL.textContent = message;
+  theWordEL.textContent = message.toUpperCase();
 };
 
 const displayWrongWord = function (message) {
@@ -53,30 +53,30 @@ const init = function () {
   arrTheWord = [...theWord];
   secretWord = [];
   calcChallenge(arrTheWord);
-  displayWord(secretWord.join(" "));
+  displayWord(secretWord.join(' '));
   displayLife(life);
   displayWrongWord();
-  displayInfo("Gissa bokstav");
-  chosenWords = ["⛔️"];
+  displayInfo('Gissa bokstav');
+  chosenWords = ['⛔️'];
   console.log(theWord);
 };
 
-ownWordEl.addEventListener("click", function () {
-  decideBarEl.classList.remove("hidden");
+ownWordEl.addEventListener('click', function () {
+  decideBarEl.classList.remove('hidden');
 });
 
-decideBarEl.addEventListener("keydown", function (e) {
-  if (e.key == "Enter" && !decideBarEl.value.length == 0) {
+decideBarEl.addEventListener('keydown', function (e) {
+  if (e.key == 'Enter' && !decideBarEl.value.length == 0) {
     theWord = decideBarEl.value.toLowerCase();
     arrTheWord = [...theWord];
     secretWord = [];
     calcChallenge(arrTheWord);
-    displayWord(secretWord.join(" "));
+    displayWord(secretWord.join(' '));
     displayLife(life);
     displayWrongWord();
-    displayInfo("Gissa bokstav");
-    chosenWords = ["⛔️"];
-    decideBarEl.classList.add("hidden");
+    displayInfo('Gissa bokstav');
+    chosenWords = ['⛔️'];
+    decideBarEl.classList.add('hidden');
   }
 });
 
@@ -85,11 +85,11 @@ decideBarEl.addEventListener("keydown", function (e) {
 ////////////////////////////////////
 init();
 
-submitBtn.addEventListener("click", function () {
+submitBtn.addEventListener('click', function () {
   guess = guessBar.value.toLowerCase();
   console.log(guess);
   test2(arrTheWord, guess);
-  guessBar.value = "";
+  guessBar.value = '';
 });
 
 /////////////////////////////////////////////
@@ -107,10 +107,11 @@ let test2 = function (arr, guess) {
     for (let i = 0; i < indices.length; i++) {
       secretWord[indices[i]] = guess;
     }
-    displayWord(secretWord.join(" "));
+    displayWord(secretWord.join(' '));
+    displayInfo('Bra bokstav!');
 
     //////////// WINNING /////////////
-    if (secretWord.join("") == theWord) {
+    if (secretWord.join('') == theWord) {
       displayWord(`Ordet är "${theWord}"!`);
     }
 
@@ -119,10 +120,11 @@ let test2 = function (arr, guess) {
     if (!chosenWords.includes(guess)) {
       life--;
       chosenWords.push(guess);
-      displayWrongWord(chosenWords.join(" "));
+      displayWrongWord(chosenWords.join(' '));
       displayLife(life);
+      displayInfo('Åh neeej!');
     } else if (chosenWords.includes(guess)) {
-      displayInfo("Redan taget!");
+      displayInfo('Redan taget!');
     } else {
       life--;
       displayLife(life);
@@ -135,4 +137,4 @@ let test2 = function (arr, guess) {
   }
 };
 
-resetBtn.addEventListener("click", init);
+resetBtn.addEventListener('click', init);
